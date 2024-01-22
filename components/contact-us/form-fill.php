f7e4544f948f23b23154f69be342267b0ddd7612
Bernard Omuse <bernardomuse22@gmail.com>
2024-01-22 09:31:47 +0300

    changes

--- components/contact-us/form-fill.php
+++ components/contact-us/form-fill.php
@@ -1,16 +1,18 @@
 <section class="relative bg-white h-fit lg:-mb-16 md:mb-0 pt-10">
-    <div class="mx-auto max-w-screen-lg">
-        <div class="flex md:flex-row flex-wrap md:ml-0 md:mr-0  ml-2 mr-2">
-            <div class="w-full md:w-1/4  p-4 -ml-4 ">
-                <h6 class=" text-3xl text-black -" id="ca-1">
-                    Drop us <br>a line
-                </h6>
-                <p class="md:mt-6">
-                    Fill in this form or <b>send us an e-mail</b><br>with your inquiry.
-                </p>
-            </div>
-            <div class="w-full md:w-3/4  md:p-4 ">
-                <?php echo do_shortcode('[forminator_form id="148"]'); ?>
+    <div class="mx-auto max-w-screen-lg md:max-w-screen-2xl">
+        <div class="ml-4 md:ml-16  sm:ml-16 md:mr-16 sm:mr-16 mr-4">
+            <div class="flex md:flex-row flex-wrap md:ml-0 md:mr-0  ml-2 mr-2">
+                <div class="w-full md:w-1/4  p-4 -ml-4 ">
+                    <h6 class=" text-3xl text-black -" id="ca-1">
+                        Drop us <br>a line
+                    </h6>
+                    <p class="md:mt-6">
+                        Fill in this form or <b>send us an e-mail</b><br>with your inquiry.
+                    </p>
+                </div>
+                <div class="w-full md:w-3/4  md:p-4 ">
+                    <?php echo do_shortcode('[forminator_form id="148"]'); ?>
+                </div>
             </div>
         </div>
     </div>
